30c24736a743c92478b69246deafac64d0af9ae3
xnliang <xnliang19@163.com>
2019-10-12 15:02:17 +0800

    fix bug

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ echo "==> Downloading glove vectors ..."
 wget http://nlp.stanford.edu/data/glove.840B.300d.zip
 
 echo "==> Unzipping glove vectors ..."
-unzip glove.840d.300d.zip
-rm glove.840d.300d.zip
+unzip glove.840B.300d.zip
+rm glove.840B.300d.zip
 
 echo "==> Done!"
